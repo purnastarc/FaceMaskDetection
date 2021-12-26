a54ef79e0e54ef08b6a790e42f7212a4fc8cf4da
Purna Chandra Pattipati <67554595+purnastarc@users.noreply.github.com>
2021-12-26 17:46:19 +0530

    Update and rename face_mask_detection_app.py to app.py

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    """ Video streaming home page """
     return render_template('index.html')
 
 
@@ -23,15 +22,15 @@ def video():
     return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
-
-
 # load our serialized face detector model from disk
 prototxtPath = r"face_detector\deploy.prototxt"
 weightsPath = r"face_detector\res10_300x300_ssd_iter_140000.caffemodel"
 faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
 
+
 # load the face mask detector model from disk
 maskNet = load_model("face_mask_detection.model")
+
 
 # initialize the video stream
 print("[INFO] starting video stream...")
@@ -40,11 +39,9 @@ print("[INFO] starting video stream...")
 def detect_and_predict_mask(frame, faceNet, maskNet):
     (h, w) = frame.shape[:2]
     blob = cv2.dnn.blobFromImage(frame, 1.0, (224, 224),(104.0, 177.0, 123.0))
-
     faceNet.setInput(blob)
     detections = faceNet.forward()
     print(detections.shape)
-
     faces = []
     locs = []
     preds = []
@@ -55,10 +52,8 @@ def detect_and_predict_mask(frame, faceNet, maskNet):
         if confidence > 0.5:
             box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
             (startX, startY, endX, endY) = box.astype("int")
-
             (startX, startY) = (max(0, startX), max(0, startY))
             (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
-
             face = frame[startY:endY, startX:endX]
             face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
             face = cv2.resize(face, (224, 224))
@@ -74,10 +69,6 @@ def detect_and_predict_mask(frame, faceNet, maskNet):
     return (locs, preds)
 
     
-     
-        
-
-
 def gen():
     vs = VideoStream(src=0).start()
     time.sleep(2.0)
@@ -89,20 +80,17 @@ def gen():
         for (box, pred) in zip(locs, preds):
             (startX, startY, endX, endY) = box
             (mask, withoutMask) = pred
-
             label = "Mask" if mask > withoutMask else "No Mask"
             color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
-
             label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
-
             cv2.putText(frame, label, (startX, startY - 10),cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
-
             cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
 		
         rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         cv2.imwrite('t.jpg', frame)
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + open('t.jpg', 'rb').read() + b'\r\n')
+	
 
 if __name__ == '__main__':
     app.run(debug=True)
