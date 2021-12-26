8af585eabd83a138365b948d84329f2a468a48cb
Purna Chandra Pattipati <67554595+purnastarc@users.noreply.github.com>
2021-12-26 17:43:35 +0530

    Update and rename train_face_mask_detector.py to model_training.py

--- model_training.py
+++ model_training.py
@@ -1,4 +1,3 @@
-# import the necessary packages
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.applications import MobileNetV2
 from tensorflow.keras.layers import AveragePooling2D
@@ -20,17 +19,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
+
 # initialize the parameters
 INIT_LR = 1e-4
 EPOCHS = 20
 BS = 32
+data = []
+labels = []
 
 DIRECTORY = r"C:\Mask Detection\CODE\Face-Mask-Detection-master\dataset" # Need to download from Kaggle
 CATEGORIES = ["with_mask", "without_mask"]
 
-
-data = []
-labels = []
 
 for category in CATEGORIES:
     path = os.path.join(DIRECTORY, category)
@@ -39,22 +38,23 @@ for category in CATEGORIES:
     	image = load_img(img_path, target_size=(224, 224))
     	image = img_to_array(image)
     	image = preprocess_input(image)
-
     	data.append(image)
     	labels.append(category)
 
+	
+	
 # one-hot encoding on the labels
 lb = LabelBinarizer()
 labels = lb.fit_transform(labels)
 labels = to_categorical(labels)
-
 data = np.array(data, dtype="float32")
 labels = np.array(labels)
-
 (trainX, testX, trainY, testY) = train_test_split(data, labels,
 	test_size=0.20, stratify=labels, random_state=42)
 
-# constructing the training image generator for data augmentation
+
+
+# data augmentation
 aug = ImageDataGenerator(
 	rotation_range=20,
 	zoom_range=0.15,
@@ -64,36 +64,30 @@ aug = ImageDataGenerator(
 	horizontal_flip=True,
 	fill_mode="nearest")
 
-# load the MobileNetV2 network
+
+# model construction
 baseModel = MobileNetV2(weights="imagenet", include_top=False,
 	input_tensor=Input(shape=(224, 224, 3)))
 
-# constructing the head of the model that will be placed on top of the
-# the base model
 headModel = baseModel.output
 headModel = AveragePooling2D(pool_size=(7, 7))(headModel)
 headModel = Flatten(name="flatten")(headModel)
 headModel = Dense(128, activation="relu")(headModel)
 headModel = Dropout(0.5)(headModel)
 headModel = Dense(2, activation="softmax")(headModel)
-
-# place the head FC model on top of the base model (this will become
-# the actual model we will train)
 model = Model(inputs=baseModel.input, outputs=headModel)
 
-# loop over all layers in the base model and freeze them so they will
-# *not* be updated during the first training process
 for layer in baseModel.layers:
 	layer.trainable = False
+	
 
-# compile our model
-print("[INFO] compiling model...")
+# compile model
 opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
 model.compile(loss="binary_crossentropy", optimizer=opt,
 	metrics=["accuracy"])
 
-# train the head of the network
-print("[INFO] training head...")
+
+# model training
 H = model.fit(
 	aug.flow(trainX, trainY, batch_size=BS),
 	steps_per_epoch=len(trainX) // BS,
@@ -102,29 +96,5 @@ H = model.fit(
 	epochs=EPOCHS)
 
 
-print("[INFO] evaluating network...")
-predIdxs = model.predict(testX, batch_size=BS)
-
-predIdxs = np.argmax(predIdxs, axis=1)
-
-
-print(classification_report(testY.argmax(axis=1), predIdxs,
-	target_names=lb.classes_))
-
-# saving the trained face mask detection model
-print("[INFO] saving mask detector model...")
+# save the model
 model.save("face_mask_detection.model", save_format="h5")
-
-# training loss and accuracy
-N = EPOCHS
-plt.style.use("ggplot")
-plt.figure()
-plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
-plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
-plt.plot(np.arange(0, N), H.history["accuracy"], label="train_acc")
-plt.plot(np.arange(0, N), H.history["val_accuracy"], label="val_acc")
-plt.title("Training Loss and Accuracy")
-plt.xlabel("Epoch #")
-plt.ylabel("Loss/Accuracy")
-plt.legend(loc="lower left")
-plt.savefig("plot.png")
